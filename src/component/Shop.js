ea3f52e0fe48d01061909b0a7603d7e849e40dfd
nehajohari92 <nehajohari92@gmail.com>
2023-11-27 15:16:15 +0530

    redux example / created action creators file and reducer file

---
+++ src/component/Shop.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Shop() {
+  return (
+    <div className="container my-3 mx-3">
+
+      <h3> Buy Item </h3>
+      <button type="button" class="btn btn-primary my-3 mx-3">
+        -
+      </button>
+      Add item into cart
+      <button type="button" class="btn btn-primary my-3 mx-3">
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Shop;
